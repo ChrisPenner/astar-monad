2fb008d9c9464b257382d2cb4921f7d98aa80b14
Chris Penner <christopher.penner@gmail.com>
2019-09-09 01:01:05 -0600

    Working

--- src/Control/Monad/AStar.hs
+++ src/Control/Monad/AStar.hs
@@ -10,24 +10,13 @@ import Data.Foldable
 data Step w a = Solved a | Continue w
     deriving Show
 
-done :: Monad m => a -> m (Step w a)
-done = return . Solved
-
-continue :: Monad m => w -> m (Step w a) -> m (Step w a)
-continue w ma = return (Continue w)
-
-handleSingle :: MonadLogic m => Maybe (Step w a, m (Step w a)) -> m (Step w a)
-handleSingle Nothing = empty
-handleSingle (Just (Solved a, _)) = return (Solved a)
-handleSingle (Just (Continue w, m)) = return (Continue w) `interleave` m
-
 weightedInterleave :: (Ord w, MonadLogic m) => m (Step w a) -> m (Step w a) -> m (Step w a)
 weightedInterleave a b = do
     rA <- msplit a
     rB <- msplit b
     case (rA, rB) of
-        (m, Nothing) -> handleSingle m
-        (Nothing, m) -> handleSingle m
+        (m, Nothing) -> reflect m
+        (Nothing, m) -> reflect m
         (Just (Solved a, _), _) -> return (Solved a)
         (_ , Just (Solved a, _)) -> return (Solved a)
         (l@(Just (Continue lw, lm)), r@(Just (Continue rw, rm))) ->
@@ -35,23 +24,27 @@ weightedInterleave a b = do
                then reflect (Just (Continue lw, weightedInterleave lm (reflect r)))
                else reflect (Just (Continue rw, weightedInterleave rm (reflect l)))
 
-
-astar :: MonadLogic m => m (Step w a) -> m a
-astar m = do
+solve :: MonadLogic m => m (Step w a) -> m a
+solve m = do
     r <- msplit m
     case r of
         Nothing -> empty
-        Just (Continue _, m) -> astar m
+        Just (Continue _, m) -> solve m
         Just (Solved a, _) -> return a
 
-findN :: MonadLogic m => Int -> Int -> m (Step Int Int)
-findN end start = do
-    if (end == start)
-       then (return (Solved start) `interleave` empty)
-       else
-            (reflect (Just (Continue $ abs (end - (start * 5)), findN end (start * 5))))
-            `weightedInterleave`
-            (reflect (Just (Continue $ abs (end - (start - 1)), findN end (start - 1))))
-            `weightedInterleave`
-            (reflect (Just (Continue $ abs (end - (start + 2)), findN end (start + 2))))
+currentCost :: MonadLogic m => w -> m (Step w a) -> m (Step w a)
+currentCost w m = reflect (Just (Continue w, m))
 
+weightedChoice :: (MonadLogic m, Ord w) => [m (Step w a)] -> m (Step w a)
+weightedChoice = foldl' weightedInterleave empty
+
+findN :: MonadLogic m => Int -> Int -> m (Step Int Int)
+findN end start = currentCost (abs $ end - start) $ do
+    if (end == start)
+       then return (Solved start)
+       else weightedChoice
+              [ findN end (start * 10)
+              , findN end (start * 3)
+              , findN end (start - 1)
+              , findN end (start + 2)
+              ]
