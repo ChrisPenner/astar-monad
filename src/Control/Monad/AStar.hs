1710d7752824ce8b4d35452da0895a37cc7ae2f9
Chris Penner <christopher.penner@gmail.com>
2019-09-12 01:09:33 -0600

    Working with new state setup

--- src/Control/Monad/AStar.hs
+++ src/Control/Monad/AStar.hs
@@ -14,11 +14,13 @@ See the <https://github.com/chrispenner/astar-monad README> for usage info and e
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Control.Monad.AStar
     (
     -- * Types
       AStar
     , AStarT
+    , BranchState (..)
 
     -- * Methods
     , MonadAStar(..)
@@ -27,14 +29,10 @@ module Control.Monad.AStar
 
     -- * Executing Search
     , runAStarT
-    , execAStarT
-    , evalAStarT
     , runAStar
-    , execAStar
-    , evalAStar
 
-    , tryWhile
-    , tryWhileT
+    -- , tryWhile
+    -- , tryWhileT
     )
     where
 
@@ -46,9 +44,17 @@ import Control.Applicative
 import Control.Monad.State
 import Data.Functor.Identity
 import Data.Maybe
+import Debug.Trace
 
-data Step c r a = Pure a | Weighted c | Solved r
+data Step c r a = Pure a | Checkpoint | Solved
     deriving (Show, Functor, Eq)
+
+data BranchState s c r =
+    BranchState { branchState :: s
+                , cost        :: c
+                , result      :: Maybe r
+                }
+                deriving (Show, Eq)
 
 -- | Non-transformer version of 'AStarT'
 type AStar s c r a = AStarT s c r Identity a
@@ -71,7 +77,7 @@ type AStar s c r a = AStarT s c r Identity a
 -- Be wary that effects will be run in seemingly non-deterministic ordering as we switch
 -- chaotically between branches.
 newtype AStarT s c r m a =
-    AStarT { unAStarT :: StateT s (LogicT m) (Step c r a)
+    AStarT { unAStarT :: StateT (BranchState s c r) (LogicT m) (Step c r a)
           } deriving stock Functor
 
 -- mapResult :: (r -> r') -> AStarT s c r m a -> AStarT s c r' m a
@@ -99,18 +105,23 @@ instance (Ord c, Monad m) => MonadFail (AStarT s c r m) where
   fail _ = empty
 
 instance (Ord c, Monad m) => MonadState s (AStarT s c r m) where
-  get = AStarT $ Pure <$> get
-  put s = AStarT $ Pure <$> put s
+  get = AStarT $ Pure . branchState <$> get
+  put s = AStarT $ Pure <$> modify (\bs -> bs{branchState=s})
 
 instance (Monad m, Ord c) => Monad (AStarT s c r m) where
   return = AStarT . return . Pure
   AStarT m >>= f = AStarT $ do
-      msplit m >>= \case
-        Nothing -> empty
-        Just (Pure a, continue) -> unAStarT $ (f a) `weightedInterleave` (AStarT continue >>= f)
-        Just (Solved r, _) -> pure $ Solved r
-        Just (Weighted c, continue) -> do
-            reflect $ Just (Weighted c, unAStarT $ AStarT continue >>= f)
+      next <- msplit m
+      bs <- get
+      case next of
+          Nothing -> empty
+          (Just (Solved, _)) -> put bs >> pure Solved
+          (Just (Pure a, continue)) -> unAStarT $ (f a) `weightedInterleave` (AStarT continue >>= f)
+          (Just (Checkpoint, continue)) ->
+                reflect $ Just (Checkpoint, unAStarT $ AStarT continue >>= f)
+
+isSolved :: BranchState c r s -> Bool
+isSolved = isJust . result
 
 -- instance (Ord c, Monad m) => MonadLogic (AStarT s c r m) where
 --   msplit (AStarT (StateT m)) = AStarT . StateT $ \s -> do
@@ -126,7 +137,7 @@ instance (Ord c, Monad m) => Alternative (AStarT s c r m) where
 weightedInterleave :: (Ord c, Monad m) => AStarT s c r m a -> AStarT s c r m a -> AStarT s c r m a
 weightedInterleave (AStarT a) (AStarT b) = AStarT $ weightedInterleave' a b
 
-weightedInterleave' :: (Ord c, MonadLogic m, MonadState s m) => m (Step c r a) -> m (Step c r a) -> m (Step c r a)
+weightedInterleave' :: (Ord c, MonadLogic m, MonadState (BranchState s c r) m) => m (Step c r a) -> m (Step c r a) -> m (Step c r a)
 weightedInterleave' ma mb = do
     beforeBoth <- get
     (rA, lState) <- liftA2 (,) (msplit ma) get
@@ -135,14 +146,14 @@ weightedInterleave' ma mb = do
     case (rA, rB) of
         (m, Nothing) -> put lState >> reflect m
         (Nothing, m) -> put rState >> reflect m
-        (Just (Solved a, _), _) -> put lState >> pure (Solved a)
-        (_ , Just (Solved a, _)) -> put rState >> pure (Solved a)
-        (l@(Just (Weighted lw, lm)), r@(Just (Weighted rw, rm)))
-          | lw < rw ->
-              (put lState >> pure (Weighted lw))
+        (Just (Solved, _), _) -> put lState >> pure Solved
+        (_, Just (Solved, _)) -> put rState >> pure Solved
+        (l@(Just (Checkpoint, lm)), r@(Just (Checkpoint, rm)))
+          | cost lState < cost rState ->do
+              (put lState >> pure Checkpoint)
                 <|> ((put lState >> lm) `weightedInterleave'` (put rState >> reflect r))
           | otherwise ->
-              (put rState >> pure (Weighted rw))
+              (put rState >> pure Checkpoint)
                <|> ((put rState >> rm) `weightedInterleave'` (put lState >> reflect l))
         ((Just (Pure la, lm)), r) ->
             (put lState >> pure (Pure la)) `interleave` ((put lState >> lm) `weightedInterleave'` (put rState >> reflect r))
@@ -150,55 +161,39 @@ weightedInterleave' ma mb = do
             (put rState >> pure (Pure ra)) `interleave` ((put rState >> rm) `weightedInterleave'` (put lState >> reflect l))
 
 -- | Run an A* computation effect returning the solution and branch state if one was found.
-runAStarT :: (Monad m) => AStarT s c r m a -> s -> m (Maybe (r, s))
+runAStarT :: (Monad m, Monoid c) => AStarT s c r m a -> s -> m (Maybe (BranchState s c r))
 runAStarT (AStarT m) s = fmap listToMaybe . observeManyT 1 $ do
-    runStateT m s >>= \case
-      (Solved a, s) -> return (a, s)
-      _ -> empty
+    runStateT m (BranchState s mempty Nothing) >>= \case
+          (Solved, s) -> return s
+          _ -> empty
 
 -- | Run a pure A* computation returning the solution and branch state if one was found.
-runAStar :: AStar s c r a -> s -> Maybe (r, s)
-runAStar m s = runIdentity $ runAStarT  m s
-
--- | Run an effectful A* computation returning only the branch state
-execAStarT :: (Monad m) => AStarT s c r m a -> s -> m (Maybe s)
-execAStarT m s = fmap snd <$> runAStarT m s
-
--- | Run an effectful A* computation returning only the solution
-evalAStarT :: (Monad m) => AStarT s c r m a -> s -> m (Maybe r)
-evalAStarT m s = fmap fst <$> runAStarT m s
-
--- | Run a pure A* computation returning only the branch state
-execAStar :: AStar s c r a -> s -> (Maybe s)
-execAStar m s = fmap snd $ runAStar m s
-
--- | Run a pure A* computation returning only the solution
-evalAStar :: AStar s c r a -> s -> (Maybe r)
-evalAStar m s = fmap fst $ runAStar m s
+runAStar :: (Monoid c) => AStar s c r a -> s -> Maybe (BranchState s c r)
+runAStar m s = runIdentity $ runAStarT m s
 
 -- | Run a pure A* search but short-circuit when the lowest cost fails a predicate.
 --
 -- This is useful for detecting if your search is diverging, or is likely to fail.
-tryWhile :: (c -> Bool) -> AStar s c r a -> s -> (Maybe (r, s))
-tryWhile p m s = runIdentity $ tryWhileT p m s
+-- tryWhile :: Monoid c => (c -> Bool) -> AStar s c r a -> BranchState s c r -> (Maybe (BranchState s c r))
+-- tryWhile p m s = runIdentity $ tryWhileT p m s
 
 -- | Effectful version of 'tryWhile'
-tryWhileT :: Monad m => (c -> Bool) -> AStarT s c r m a -> s -> m (Maybe (r, s))
-tryWhileT p m s = do
-    stepAStar m s >>= \case
-      Nothing -> return Nothing
-      Just ((Pure _, s), continue) -> tryWhileT p continue s
-      Just ((Weighted c, s), continue) ->
-          if p c then tryWhileT p continue s
-                 else return Nothing
-      Just ((Solved r, s), _) -> return (Just (r, s))
+-- tryWhileT :: (Monoid c, Monad m) => (c -> Bool) -> AStarT s c r m a -> BranchState s c r -> m (Maybe (BranchState s c r))
+-- tryWhileT p m s = do
+--     stepAStar m s >>= \case
+--       Nothing -> return Nothing
+--       Just ((Pure _, s), continue) -> tryWhileT p continue s
+--       Just ((Weighted c, s), continue) ->
+--           if p c then tryWhileT p continue s
+--                  else return Nothing
+--       Just ((Solved r, s), _) -> return (Just s)
 
 -- NOTE probably doesn't handle state properly in returned continuation...
-stepAStar :: (Monad m) => AStarT s c r m a -> s -> m (Maybe ((Step c r a, s), AStarT s c r m a))
+stepAStar :: (Monoid c, Monad m) => AStarT s c r m a -> BranchState s c r -> m (Maybe ((Step c r a, BranchState s c r), AStarT s c r m a))
 stepAStar (AStarT m) s = fmap (fmap go) . observeT . (fmap . fmap . fmap . fmap) fst $ msplit (runStateT m s)
   where
     go (v, x) = (v, AStarT (lift x))
 
 instance (Ord w, Monad m) => MonadAStar w r (AStarT s w r m) where
-  updateCost c = AStarT $ pure (Weighted c) <|> return (Pure ())
-  done = AStarT . pure . Solved
+  updateCost c = AStarT $ modify (\bs -> bs{cost=c}) >> (pure Checkpoint <|> pure (Pure ()))
+  done r = AStarT $ modify (\bs -> bs{result=Just r}) >> pure Solved
--- test/Spec.hs
+++ test/Spec.hs
@@ -11,6 +11,7 @@ import Data.Foldable
 import Control.Lens hiding (Context)
 import Control.Monad.State
 import Control.Applicative
+import Data.Monoid
 
 data Move = U | D | L | R
     deriving (Show, Eq)
@@ -28,13 +29,13 @@ main :: IO ()
 main = hspec $ do
     describe "a-star" $ do
         it "should find a solution" $ do
-            (view moves . snd <$> runAStar findPoint (Context (3, 6) (5, 5) []))
-              `shouldBe` Just ([U, R, R])
+            (join . fmap result $ runAStar findPoint (Context (3, 6) (5, 5) []))
+              `shouldBe` Just (5, 5)
         it "should take the shortest path" $ do
-            (view moves . snd <$> runAStar findPoint (Context (4, 6) (5, 5) []))
-              `shouldBe` (Just [U, R])
+            (view moves . branchState <$> runAStar findPoint (Context (4, 6) (5, 5) []))
+              `shouldBe` Just [U, R]
         it "should take the shortest path in long situations" $ do
-            (length . view moves . snd <$> runAStar findPoint (Context (4, 6) (20, 20) []))
+            (length . view moves . branchState <$> runAStar findPoint (Context (4, 6) (20, 20) []))
               `shouldBe` Just 30
         -- it "should properly rewind state" $ do
         --       do flip execAStar [] $ do
@@ -42,39 +43,39 @@ main = hspec $ do
         --                  , updateCost (2 :: Int) >> modify (++ [2]) >> done ()
         --                  ]
         it "should resolve with Nothing if branches return after updating cost" $ do
-              do flip evalAStar () $ (updateCost @Int 1 >> return ()) <|> return ()
+              do flip runAStar () $ (updateCost 1 >> return ()) <|> return ()
             `shouldBe`
-              (Nothing :: Maybe ())
+              (Nothing :: Maybe (BranchState () (Sum Int) ()))
         it "should resolve with solution if some branches simply return" $ do
-              do flip evalAStar () $ (return () <|> (updateCost @Int 1 >> done ()))
+              do flip runAStar () $ (return () <|> (updateCost 1 >> done ()))
             `shouldBe`
-              Just ()
-        it "should resolve with solution if all branches simply return" $ do
-              do flip evalAStar () $ (return () <|> return () :: AStar () () () ())
+              Just (BranchState () (Sum 1) (Just ()))
+        it "should resolve without solution if all branches simply return" $ do
+              do flip runAStar () $ (return () <|> return () :: AStar () () () ())
             `shouldBe`
-              Nothing
-    describe "tryWhile" $ do
-        it "should stop if weight gets too high" $ do
-              -- Use tuple monad to see how far we get
-              do flip (tryWhileT (< 4)) () $ do
-                    asum [ updateCost (10 :: Int) >> lift ([10], ()) >> empty
-                          , updateCost (1 :: Int) >> lift ([1], ()) >> empty
-                          , updateCost (5 :: Int) >> lift ([5], ()) >> empty
-                          , updateCost (3 :: Int) >> lift ([3], ()) >> empty
-                         ]
-            `shouldBe`
-              ([1, 3] :: [Int], Nothing :: Maybe ((), ()))
+              (Nothing :: Maybe (BranchState () () ()))
+    -- describe "tryWhile" $ do
+    --     it "should stop if weight gets too high" $ do
+    --           -- Use tuple monad to see how far we get
+    --           do flip (tryWhileT (< 4)) () $ do
+    --                 asum [ updateCost (10 :: Int) >> lift ([10], ()) >> empty
+    --                       , updateCost (1 :: Int) >> lift ([1], ()) >> empty
+    --                       , updateCost (5 :: Int) >> lift ([5], ()) >> empty
+    --                       , updateCost (3 :: Int) >> lift ([3], ()) >> empty
+    --                      ]
+    --         `shouldBe`
+    --           ([1, 3] :: [Int], Nothing :: Maybe ((), ()))
 
 
 distanceTo :: (Int, Int) -> (Int, Int) -> Int
 distanceTo (x, y) (x', y') = abs (x - x') + abs (y - y')
 
-findPoint :: AStar Context Int () ()
+findPoint :: AStar Context (Sum Int) (Int, Int) ()
 findPoint = do
     c <- use current
     gl <- use goal
-    when (c == gl) $ done ()
-    updateCost $ distanceTo gl c
+    when (c == gl) $ done gl
+    updateCost . Sum $ distanceTo gl c
     asum
         [ moves <>= [R] >> current . _1 += 1 >> findPoint
         , moves <>= [L] >> current . _1 -= 1 >> findPoint
